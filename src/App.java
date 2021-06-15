cfc1a2de8c8428b258806ef2ff92cb315c3d6ffd
alexcortes22 <alex.cortes47@qmail.cuny.edu>
2021-06-15 13:32:03 -0400

    added hashmap implementation to keep track of duplicates

--- src/App.java
+++ src/App.java
@@ -3,7 +3,7 @@ import codingProblems.*;
 public class App {
     public static void main(String[] args) throws Exception {
         //System.out.println("Hello, World!");
-        int[] nums = {2, 3, 3, 0, 0};
+        int[] nums = {-10, 12, -20, -8, 15};
         boolean result = doubleSearch.checkIfExist(nums);
 
         System.out.println("N and its double exists: " + result);
--- src/codingProblems/doubleSearch.java
+++ src/codingProblems/doubleSearch.java
@@ -1,17 +1,29 @@
 package codingProblems;
 import java.util.Arrays;
+import java.util.HashMap;
 
 
 public class doubleSearch {
     public static boolean checkIfExist(int[] arr) {
+        HashMap<Integer, Integer> doubleNumbers = new HashMap<Integer, Integer>(arr.length);
         Arrays.sort(arr);
         boolean found = false;
-        
+
+        //O(n)
+        for(int number : arr){
+           if(doubleNumbers.get(number) == null){
+               doubleNumbers.put(number, 1);
+           }else{
+               doubleNumbers.put(number, doubleNumbers.get(number) + 1);
+           }
+        }
+
+        //O(n)
         for(int i = 0; i < arr.length; i++){
             int doubleNum = arr[i] * 2;
             int result = binarySearch(arr, doubleNum, 0, arr.length - 1);
             
-            if(result >= 0){
+            if(result >= 0 && doubleNumbers.get(doubleNum) == 1){
                 found = true;
                 break;
             }

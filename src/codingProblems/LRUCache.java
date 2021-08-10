7cafe97294c84224fc4c479e90081c9c1a448c63
Alex Cortes <alex.cortes2137@gmail.com>
2021-08-09 20:04:55 -0400

    LRU Cache Implementation

--- src/codingProblems/LRUCache.java
+++ src/codingProblems/LRUCache.java
@@ -3,56 +3,78 @@ package codingProblems;
 import java.util.HashMap;
 
 class LRUCache{
-    //Properties
-    //This HashMap maps a priority to the key in the cache
-    HashMap <Integer, Integer> priorities = new HashMap <Integer, Integer>();
-    Pair[] cache;
+    HashMap<Integer, Node> cache = new HashMap<Integer, Node>();
+    Node head, tail;
     int size;
-    int head = -1;
-    int tail = -1;
-    int recentCounter;
+    int capacity;
+
+    class Node{
+        int key;
+        int value;
+        Node next;
+        Node prev;
+    }
+
+    private void addNode(Node node){
+        node.prev = head;
+        node.next = head.next;
+        head.next.prev = node;
+        head.next = node;
+    }
+
+    private void removeNode(Node node){
+        Node previous = node.prev;
+        Node next = node.next;
+        previous.next = next;
+        next.prev = previous;
+    }
+
+    private void moveToHead(Node node){
+        removeNode(node);
+        addNode(node);
+    }
+
+    private Node removeTail(){
+        Node res = tail.prev;
+        removeNode(res);
+        return res;
+    }
 
     public LRUCache(int capacity){
-        this.size = capacity;
-        cache = new Pair[size];
-        recentCounter = 0;
+        this.size = 0;
+        this.capacity = capacity;
+        head = new Node();
+        tail = new Node();
     }
 
     public int get(int key){
-       
+       Node result = cache.get(key);
+       if(result == null){
+           return -1;
+       }
+       return result.value;
     }
 
     public void put(int key, int value){
-        //check if the pair is in the array O(1)
-        if(priorities.containsValue(key)){
-            
+        Node result = cache.get(key);
+        if(result == null){
+            Node newNode = new Node();
+            newNode.key = key;
+            newNode.value = value;
+            cache.put(key, newNode);
+            addNode(newNode);
+            size++;
+
+            if(size > capacity){
+                Node tail = removeTail();
+                cache.remove(tail.key);
+                --size;
+            }
+        }else{
+            result.value = value;
+            moveToHead(result);
         }
     }
 
-    static class Pair {
-        int key;
-        int value;
-
-        public Pair(int key, int value){
-            this.key = key;
-            this.value = value;
-        } 
-        
-        public void setKey(int newKey){
-            this.key = newKey;
-        }
-
-        public void setValue(int newValue){
-            this.value = newValue;
-        }
-
-        public int getKey(){
-            return this.key;
-        }
-
-        public int getValue(){
-            return this.value;
-        }
-    } 
 }
 
